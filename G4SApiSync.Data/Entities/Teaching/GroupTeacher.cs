f5c08cd717b273a48d62bc6da20f24785217f58a
Ben Dobbs <bdobbs@archwaytrust.co.uk>
2025-12-12 12:05:59 +0000

    Added entity description for GroupTeacher.

---
+++ G4SApiSync.Data/Entities/Teaching/GroupTeacher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace G4SApiSync.Data.Entities
+{
+    public class GroupTeacher
+    {
+        [MaxLength(100)]
+        public string GroupId { get; set; } //AcademyCode + DataSet + "-" + G4SGroupId
+
+        [MaxLength(100)]
+        public string TeacherId { get; set; } //AcademyCode + DataSet + "-" + G4STeacherId
+
+        public virtual Teacher Teacher { get; set; }
+
+        public virtual Group Group { get; set; }
+    }
+}
