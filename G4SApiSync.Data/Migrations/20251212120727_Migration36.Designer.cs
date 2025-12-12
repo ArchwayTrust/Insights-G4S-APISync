305925785f2f0ee6ee4f0daf39d73c9c0afcbdca
Ben Dobbs <bdobbs@archwaytrust.co.uk>
2025-12-12 12:09:23 +0000

    Added database migration.

--- G4SApiSync.Data/Migrations/20251212120727_Migration36.Designer.cs
+++ G4SApiSync.Data/Migrations/20251212120727_Migration36.Designer.cs
@@ -4,6 +4,7 @@ using G4SApiSync.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace G4SApiSync.Data.Migrations
 {
     [DbContext(typeof(G4SContext))]
-    partial class G4SContextModelSnapshot : ModelSnapshot
+    [Migration("20251212120727_Migration36")]
+    partial class Migration36
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
